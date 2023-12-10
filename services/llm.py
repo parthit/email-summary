9b612a16cddd3dc7271606e962a128a22c13ee8a
Parthit Patel <pbp38@cornell.edu>
2023-12-09 23:40:48 -0500

    modified prompt

--- services/llm.py
+++ services/llm.py
@@ -10,6 +10,7 @@ client = openai.OpenAI(
 
 def get_email_prompt(email):
         email_prompt = f"""
+        You are an assistant to a Software Engineer.
         For the given email body, try to fill out the following template.
         All output should be in JSON format.
         If dates/days are mentioned in the email, prioritise mentioning them.
@@ -28,6 +29,8 @@ def get_email_prompt(email):
         Add UrgGPT to subject if email is urgent.
         Add ResLtrGPT to subject if email can be read later and is not important.
 
+        All are in context to Software Engineers. 
+
         Your output should look like:
         OUTPUT: {{"subject": <ImpGPT/UrgGPT/ResLtrGPT : SUBJECT_DATA>, "body": "<EMAIL_SUMMARY>"}}
         """
@@ -43,7 +46,7 @@ def make_oai_call(email, model="gpt-3.5-turbo-1106"):
         messages=[
             {
                 "role": "system",
-                "content": "You are a meeting assistant which reviews and summarises emails into 30 words or less.",
+                "content": "You are a meeting assistant to a Software Engineer who reviews and summarises emails into 30 words or less.",
             },
             {"role": "user", "content": get_email_prompt(email)},
         ],
